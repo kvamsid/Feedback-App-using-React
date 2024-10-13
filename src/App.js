bae605f9ccfd2c17cddffe69f201dd4fe39e5e88
kvamsid <vamsikanakavety144@gmail.com>
2024-10-12 19:04:56 -0700

    Added Feature of adding and removing ratings

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import { v4 as uuidv4} from 'uuid'
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData)
   const deleteFeedback = (id) => {
     if(window.confirm('Are you sure you want to delete?')){
-      setFeedback(feedback.filter((item) => item.id != id))
+      setFeedback(feedback.filter((item) => item.id !== id))
     }
-    
   }
+
+
+  const addFeedback = (newFeedback) =>{
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+
+  }
+
   return (
     <>
     <Header/>
     <div className = 'container' >
+      <FeedbackForm handleAdd = {addFeedback}></FeedbackForm>
         <FeedbackStats feedback = {feedback}/>
         <FeedbackList feedback = {feedback} handleDelete = {deleteFeedback}/>
     </div>
